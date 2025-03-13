eed4447044078ced832296a589b1576eab8847db
Nicolas <nico.dobosz@gmail.com>
2025-03-13 15:30:06 +0100

    map work with a marker

--- app/src/main/java/com/example/toilets_finder/MapActivity.kt
+++ app/src/main/java/com/example/toilets_finder/MapActivity.kt
@@ -1,23 +1,42 @@
 package com.example.toilets_finder
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
 
 class MapActivity : AppCompatActivity() {
+
+    private lateinit var map: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_map)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Configuration d'OSMDroid
+        Configuration.getInstance().userAgentValue = packageName
+
+        // Initialisation de la carte
+        map = findViewById(R.id.map)
+        map.setMultiTouchControls(true)
+
+        // Centrer la carte sur une position (ex: Paris)
+        val startPoint = GeoPoint(48.980494, 2.300834)
+        map.controller.setZoom(12.0)
+        map.controller.setCenter(startPoint)
+
+        // Ajouter un marqueur
+        val marker = Marker(map)
+        marker.position = startPoint
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        marker.title = "Maison :)"
+        map.overlays.add(marker)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        map.onDetach() // Nettoyage de la carte pour éviter les fuites de mémoire
     }
 }
