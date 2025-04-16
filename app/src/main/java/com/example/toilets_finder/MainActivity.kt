616101ea4587df651769b2deee3c3b76ffd56e56
KasutoSoei <dimanchegwendal@gmail.com>
2025-04-16 14:34:19 +0200

    progress bar in main menu while fetching API data, then redirect to map

--- app/src/main/java/com/example/toilets_finder/MainActivity.kt
+++ app/src/main/java/com/example/toilets_finder/MainActivity.kt
@@ -4,12 +4,29 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var loadingProgressBar: ProgressBar
+    private lateinit var loadingProgressText: TextView
+    private lateinit var viewMapButton: Button
+    private var totalRequests = 7
+    private var responsesReceived = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,13 +36,94 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val buttonMap = findViewById<Button>(R.id.mapButton)
-        buttonMap.setOnClickListener(View.OnClickListener {
-            val intent = Intent(
-                this,
-                MapActivity::class.java
-            )
-            startActivity(intent)
+
+        ToiletDataStore.toiletList.clear()
+
+        loadingProgressBar = findViewById<ProgressBar>(R.id.loadingProgressBar)
+        loadingProgressText = findViewById<TextView>(R.id.loadingProgressTextView)
+        viewMapButton = findViewById<Button>(R.id.mapButton)
+
+
+        viewMapButton.setOnClickListener(View.OnClickListener {
+            loadingProgressBar.visibility = View.VISIBLE
+            loadingProgressText.visibility = View.VISIBLE
+            loadingProgressBar.progress = 0
+            loadingProgressText.text = "Loading map : 0%"
+            viewMapButton.visibility = View.GONE
+            fetchAllToiletsData()
         })
+
+    }
+
+    // Create the 7 API URLs we need.
+    // The API can only provide data 100 results by 100, so for 623 results we need 7 requests
+    private fun fetchAllToiletsData() {
+        for (i in 0 until totalRequests) {
+            val apiUrl =
+                "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/sanisettesparis/records?limit=100&offset=" + 100 * i
+            print(apiUrl)
+            fetchToiletData(apiUrl)
+        }
+    }
+
+    private fun fetchToiletData(apiUrl: String) {
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val url = URL(apiUrl)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+
+                if (connection.responseCode == 200) {
+                    val response = connection.inputStream.bufferedReader().use { it.readText() }
+                    val jsonResponse = JSONObject(response)
+                    val records = jsonResponse.getJSONArray("results")
+
+                    for (i in 0 until records.length()) {
+                        val record = records.getJSONObject(i)
+
+                        if (!record.isNull("geo_point_2d")) {
+                            val geoPoint = record.getJSONObject("geo_point_2d")
+                            val lat = geoPoint.getDouble("lat")
+                            val lon = geoPoint.getDouble("lon")
+                            val address = record.getString("adresse")
+
+                            ToiletDataStore.toiletList.add(Triple(lat, lon, address))
+                        }
+                        else {
+                            println("Record $i doesn't have geo_point_2d")
+                        }
+                    }
+
+                    withContext(Dispatchers.Main) {
+                        responsesReceived++
+                        val progress = (responsesReceived * 100) / totalRequests
+                        println("Progress: $progress")
+                        loadingProgressBar.progress = progress
+                        loadingProgressText.text = "Map loading : $progress%"
+
+                        if (responsesReceived == totalRequests) {
+                            println("All data received, launching MapActivity")
+                            if (!isFinishing && !isDestroyed) {
+                                val intent = Intent(this@MainActivity, MapActivity::class.java)
+                                startActivity(intent)
+                            }
+                        }
+                    }
+                }
+                else {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@MainActivity, "Connection error", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+            catch (e: Exception) {
+                e.printStackTrace()
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, "Error while fetching data", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/toilets_finder/MapActivity.kt
+++ app/src/main/java/com/example/toilets_finder/MapActivity.kt
@@ -5,26 +5,21 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.drawable.BitmapDrawable
-import android.graphics.Bitmap
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import kotlinx.coroutines.*
-import org.json.JSONObject
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
-import java.net.HttpURLConnection
-import java.net.URL
+import androidx.core.graphics.scale
+import androidx.core.graphics.drawable.toDrawable
 
 class MapActivity : AppCompatActivity(), LocationListener {
 
@@ -35,6 +30,7 @@ class MapActivity : AppCompatActivity(), LocationListener {
     private val MIN_TIME_UPDATE: Long = 1000
     private val MIN_DISTANCE_UPDATE: Float = 5f
 
+
     // Default coordinates (center of Paris, Notre-Dame)
     private val DEFAULT_LATITUDE = 48.8566
     private val DEFAULT_LONGITUDE = 2.3522
@@ -43,7 +39,8 @@ class MapActivity : AppCompatActivity(), LocationListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        // get the "Back" button, for going back to the home
+
+        // get the "Back" button, to go back to the home page
         val buttonBack: Button = findViewById(R.id.backButton)
         buttonBack.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
@@ -56,14 +53,14 @@ class MapActivity : AppCompatActivity(), LocationListener {
         // Uncomment/Comment the line below for hide/show the zoom buttons
         // map.zoomController.setVisibility(org.osmdroid.views.CustomZoomButtonsController.Visibility.NEVER)
 
-        // Check is the user location is allowed, else ask it
+        // Check if the user location is allowed, else ask it
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             setupLocation()
         } else {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
         }
 
-        getApiUrl()
+        addAllToiletsMarkers()
     }
 
     private fun setupLocation() {
@@ -76,7 +73,7 @@ class MapActivity : AppCompatActivity(), LocationListener {
 
             // Check if GPS or Network are enabled and get the position of the user, if both are not enabled display an error.
             if (!isGpsEnabled && !isNetworkEnabled) {
-                Toast.makeText(this, "Aucun service de localisation disponible", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "No location service available", Toast.LENGTH_SHORT).show()
                 updateMapLocation(DEFAULT_LATITUDE, DEFAULT_LONGITUDE)
             } else {
                 if (isGpsEnabled) {
@@ -109,38 +106,39 @@ class MapActivity : AppCompatActivity(), LocationListener {
         }
     }
 
-    // Delete the last position of the user and add the new position
+    // Update the map with the new user's position, based on his coordinates
     private fun updateMapLocation(latitude: Double, longitude: Double) {
-        // Actually the coordinate is in the center of Paris.
-        // If you want the truth coordinate of your device, replace DEFAULT_LATITUDE and DEFAULT_LATITUDE bye latitude and longitude
-        val userPoint = GeoPoint(DEFAULT_LATITUDE, DEFAULT_LONGITUDE)
+        // By default the coordinates are the center of Paris
+        // If you want the true coordinates of your device, replace DEFAULT_LATITUDE and DEFAULT_LONGITUDE by latitude and longitude
+        val userPoint = GeoPoint(latitude, longitude)
         map.controller.setZoom(15.0)
         map.controller.setCenter(userPoint)
 
-        map.overlays.removeAll { it is Marker && it.title == "Votre position" }
+        // Remove the last user position marker
+        map.overlays.removeAll { it is Marker && it.title == "You are here" }
 
         val userMarker = Marker(map)
         userMarker.position = userPoint
         userMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        userMarker.title = "Votre position"
+        userMarker.title = "You are here"
         map.overlays.add(userMarker)
     }
 
     // Function from OSM (the map)
-    // The function is called every MIN_TIME_UPDATE and MIN_DISTANCE_UPDATE
+    // This function is called every MIN_TIME_UPDATE and MIN_DISTANCE_UPDATE
     override fun onLocationChanged(location: Location) {
         updateMapLocation(location.latitude, location.longitude)
     }
 
     // Function from OSM (the map)
-    // The function is handling the response of the user when we ask him for the localisation permissions
+    // This function is handling the response of the user when we ask him for the localisation permissions
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 setupLocation()
             } else {
-                Toast.makeText(this, "Permission refusée", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
                 updateMapLocation(DEFAULT_LATITUDE, DEFAULT_LONGITUDE)
             }
         }
@@ -153,56 +151,28 @@ class MapActivity : AppCompatActivity(), LocationListener {
         locationManager.removeUpdates(this)
     }
 
-    // Create the 7 API Url we need.
-    // The API can only give the data 100 by 100, for 623 we need 7 requests
-    private fun getApiUrl() {
-        for (i in 0 until 7) {
-            val apiUrl = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/sanisettesparis/records?limit=100&offset=" + 100 * i
-            fetchToiletData(apiUrl)
+
+    // Adds on the map markers for all the toilets, from the data stored in ToiletDataStore
+    private fun addAllToiletsMarkers() {
+        for (toilet in ToiletDataStore.toiletList) {
+            val lat = toilet.first
+            val lon = toilet.second
+            val address = toilet.third
+            addToiletMarker(lat, lon, address)
         }
     }
 
-    private fun fetchToiletData(apiUrl: String) {
 
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val url = URL(apiUrl)
-                val connection = url.openConnection() as HttpURLConnection
-                connection.requestMethod = "GET"
-
-                if (connection.responseCode == 200) {
-                    val response = connection.inputStream.bufferedReader().use { it.readText() }
-                    val jsonResponse = JSONObject(response)
-                    val records = jsonResponse.getJSONArray("results")
-
-                    withContext(Dispatchers.Main) {
-                        for (i in 0 until records.length()) {
-                            val record = records.getJSONObject(i)
-                            val geoPoint = record.getJSONObject("geo_point_2d")
-                            val lat = geoPoint.getDouble("lat")
-                            val lon = geoPoint.getDouble("lon")
-                            val address = record.getString("adresse")
-
-                            addToiletMarker(lat, lon, address)
-                        }
-                    }
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-    }
-
-    // Add a marker on the map with the latitude and longitude in params.
-    // Change the base icon of the marker for a custom icon ans resize it.
+    // Adds a marker on the map with the latitude and longitude in params.
+    // Change the base icon of the marker with a custom icon and resize it.
     private fun addToiletMarker(lat: Double, lon: Double, address: String) {
         val toiletMarker = Marker(map)
         toiletMarker.position = GeoPoint(lat, lon)
         toiletMarker.title = address
 
         val customIcon = ContextCompat.getDrawable(this, R.drawable.ic_toilet_marker) as BitmapDrawable
-        val resizedIcon = Bitmap.createScaledBitmap(customIcon.bitmap, 35, 58, false)
-        toiletMarker.icon = BitmapDrawable(resources, resizedIcon)
+        val resizedIcon = customIcon.bitmap.scale(35, 58, false)
+        toiletMarker.icon = resizedIcon.toDrawable(resources)
 
         map.overlays.add(toiletMarker)
     }
